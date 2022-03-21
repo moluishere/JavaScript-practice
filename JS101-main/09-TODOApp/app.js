3c42f3540be45ae4b69344e22d4d13c51670eb1c
Yvonne <moluishere@gmail.com>
2022-03-21 16:07:16 +0800

    TODOapp 作業修正與範例

--- JS101-main/09-TODOApp/app.js
+++ JS101-main/09-TODOApp/app.js
@@ -1,20 +1,19 @@
 // 鍵盤事件 enter(keycode 13 = Enter)：按 enter 新增
 // click 事件：點選 ddBtn 新增，點選 closeBtn 刪除
-
 const ul = document.createElement("ul")
 ul.classList.add("todo-list")
+document.querySelector(".todo-app").appendChild(ul)
 
 function addNewList() {
-  document.querySelector(".todo-app").appendChild(ul)
   const li = document.createElement("li")
   li.classList.add("todo-item")
   const inputValue = document.querySelector("#taskInput").value
-  if (inputValue.trim().length === 0) {
+  if (inputValue.trim() === "") {
     alert("寫資料啊")
   } else {
     li.innerHTML = `
-  <span class="item">${inputValue}</span>
-  <button class="closeBtn">X</button>`
+    <span class="item">${inputValue}</span>
+    <button class="closeBtn">X</button>`
     ul.insertAdjacentElement("afterbegin", li)
     document.querySelector("#taskInput").value = " "
   }
@@ -24,13 +23,13 @@ function addNewList() {
   })
 }
 
-// 點擊 button (ddBtn) -> 新增資料
+// 點擊btn 新增資料
 const add = document.querySelector("#addBtn")
 add.addEventListener("click", () => {
   addNewList()
 })
 
-// 按 enter -> 新增資料
+// 按 enter 新增資料
 const enter = document.querySelector("#taskInput")
 enter.addEventListener("keypress", (e) => {
   if (e.which === 13) {
